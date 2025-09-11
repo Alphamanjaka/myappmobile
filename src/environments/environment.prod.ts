a27025025cbfc7b852bb6444c9b964a8d7ede98c
alphamanjaka <alphamanjaka@gmail.com>
2025-09-11 22:40:20 +0300

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: ''
+  apiUrl: 'https://travel-agency-2er9.onrender.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://travel-agency-2er9.onrender.com/api'
 };
 
 /*
