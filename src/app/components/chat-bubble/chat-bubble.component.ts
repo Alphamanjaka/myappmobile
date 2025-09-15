daa8251b5b4f34c600580aa14347429a8b571414
alphamanjaka <alphamanjaka@gmail.com>
2025-09-15 09:20:47 +0300

    update

--- src/app/components/chat-bubble/chat-bubble.component.ts
+++ src/app/components/chat-bubble/chat-bubble.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { CommonModule } from '@angular/common';
+
 import { ChatbotComponent } from '../chatbot/chatbot.component';
 
 @Component({
   selector: 'app-chat-bubble',
   templateUrl: './chat-bubble.component.html',
   standalone: true,
-  imports: [IonicModule, CommonModule, ChatbotComponent],
+  imports: [IonicModule, ChatbotComponent],
 })
 export class ChatBubbleComponent {
   isOpen = false;
